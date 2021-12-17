557a7c096453aba5283f856628bd91a8e0723c46
Daniel Tobón Uribe <daniel.tobon@nebulae.com.co>
2021-12-17 13:12:28 -0500

    Se agregan los metodos necesarios para el api client

--- api/client-gateway/graphql/business-management/resolvers.js
+++ api/client-gateway/graphql/business-management/resolvers.js
@@ -13,6 +13,7 @@ const { mergeMap, catchError, map } = require("rxjs/operators");
 const INTERNAL_SERVER_ERROR_CODE = 1;
 const BUSINESS_PERMISSION_DENIED_ERROR_CODE = 2;
 
+
 function getResponseFromBackEnd$(response) {
   return of(response).pipe(
     map(resp => {
